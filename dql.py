120dd4aab380835809676b151fa1a8f91c54cbd3
Alexandre Thibault <xandre.thib@gmail.com>
2023-06-23 08:49:52 +0200

    Ajout appr sur N frales (models mauvais)

--- dql.py
+++ dql.py
@@ -7,15 +7,26 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from tqdm import tqdm
 from collections import deque
+from dataclasses import dataclass
+import cv2
+import pandas as pd
+from matplotlib import pyplot as plt
+import csv
 
 SAVE_PATH = "./models"
 GAMES = {"space_invaders": "ALE/SpaceInvaders-v5", "breakout": "ALE/Breakout-v5"}
-MAX_FRAMES = 20000
-MAX_BUFFER = 25
-NB_FRAME = 4
+MAX_FRAMES = 50000
+MAX_BUFFER = 30
+NB_FRAME = 3
 
 # Configuration paramaters for the whole setup
 seed = 42
+
+@dataclass
+class Stats:
+    episode: np.ndarray
+    score: np.ndarray
+    steps_survived: np.ndarray
 
 
 def make_model(input_shape, n_actions, version=2):
@@ -40,7 +51,7 @@ def make_model(input_shape, n_actions, version=2):
 
     model = keras.Model(inputs=inputs, outputs=action)
     model.compile(
-        keras.optimizers.Adam(learning_rate=0.01), keras.losses.MeanSquaredError()
+        keras.optimizers.Adam(learning_rate=0.001), keras.losses.MeanSquaredError()
     )
     return model
 
@@ -48,9 +59,26 @@ def make_model(input_shape, n_actions, version=2):
 def save(model, name, version):
     model.save(f"{SAVE_PATH}/{name}_v{version}")
 
-
 def load(name: str):
     return tf.keras.models.load_model(f"{SAVE_PATH}/{name}")
+
+def save_stats(stats: Stats, name, version):
+    fields = list(stats.__dict__.keys())
+    with open(f"{SAVE_PATH}/{name}_v{version}/stats.csv", "w") as f:
+        write = csv.writer(f)
+        write.writerow(fields)
+        write.writerows(np.array(list(stats.__dict__.values())).T)
+
+def plot_stats(name: str, version: int):
+    df = pd.read_csv(f"./models/{name}_v{version}/stats.csv")
+    fig, ax = plt.subplots(2)
+    ax[0].plot(df.episode, df.score)
+    ax[1].plot(df.episode, df.steps_survived)
+    plt.show()
+
+def downscale(img: np.ndarray, factor: float) -> np.ndarray:
+    y_size, x_size, _ = img.shape
+    return cv2.resize(img, dsize=(int(x_size // factor), int(y_size // factor)))
 
 def calc_reward_mean(hisory_buffer, center = 13, dist = 8):
     res = 0
@@ -72,19 +100,28 @@ def train(name: str, version: int = 2, render: bool = False):
     epsilon_decay = 0.9 / exploration_frames
 
     n_actions = int(env.action_space.n)
-    space_shape = env.observation_space.shape
-    model = make_model(space_shape, n_actions, version)
+    
 
     history_buffer = []
     frames_observation = deque(maxlen=NB_FRAME)
     nb_buffer = 0
 
     observation, info = env.reset(seed=seed)
-    frames_observation.append(observation)
+    frames_observation.append(downscale(observation, 2))
+
+    space_shape = downscale(observation, 2).shape
+    model = make_model(space_shape, n_actions, version)
+
+    stats = Stats(np.array([0]), np.array([0]), np.array([0]))
+    running_reward = 0
+    steps_survived = 0
+
     for k in range(NB_FRAME-1):
         action = np.random.randint(n_actions)
         next_observation, reward, terminated, truncated, info = env.step(action)
-        frames_observation.append(next_observation)
+        frames_observation.append(downscale(next_observation, 2))
+        running_reward += reward
+        steps_survived += 1
 
     for i in tqdm(range(MAX_FRAMES)):
         q_values = model(np.array([list(frames_observation)]))[0].numpy()
@@ -97,7 +134,10 @@ def train(name: str, version: int = 2, render: bool = False):
             action = np.argmax(q_values)
 
         next_observation, reward, terminated, truncated, info = env.step(action)
-        frames_observation.append(next_observation)
+        frames_observation.append(downscale(next_observation, 2))
+
+        running_reward += reward
+        steps_survived += 1
 
 
         # Append history_beffer with new observation
@@ -106,7 +146,14 @@ def train(name: str, version: int = 2, render: bool = False):
 
         if terminated:
             observation, info = env.reset(seed=seed)
-            frames_observation.append(next_observation)
+            frames_observation.append(downscale(observation, 2))
+
+            stats.episode = np.append(stats.episode, stats.episode[-1] + 1)
+            stats.score = np.append(stats.score, running_reward)
+            stats.steps_survived = np.append(stats.steps_survived, steps_survived)
+            running_reward = 0
+            steps_survived = 0
+
             continue
 
         #next_q_values = model(np.array([next_observation]))[0].numpy()
@@ -115,7 +162,7 @@ def train(name: str, version: int = 2, render: bool = False):
             observation_temp, action_temp, _ = history_buffer.pop(0)
             #####
             next_q_values = model(np.array([list(frames_observation)]))[0].numpy()
-            reward_temp = calc_reward_mean(history_buffer, 13, 8)
+            reward_temp = calc_reward_mean(history_buffer, 14, 9)
             nb_buffer -= 1
             update_q_values = q_values
             update_q_values[action_temp] = reward_temp + gamma * np.max(next_q_values)
@@ -126,8 +173,11 @@ def train(name: str, version: int = 2, render: bool = False):
 
         if not i%5000:
             save(model, name, version=version)
+            save_stats(stats, name, version)
             print("model saved")
+
     save(model, name, version=version)
+    save_stats(stats, name, version)
 
 def test(name: str, version: int):
     def quit(env, sig, frame):
@@ -145,7 +195,7 @@ def test(name: str, version: int):
 
     while True:
         observation, _ = env.reset()
-        frames_observation.append(observation)
+        frames_observation.append(downscale(observation, 2))
         terminated = False
         while not terminated:
             if frames_observation.maxlen == len(frames_observation):
@@ -153,10 +203,10 @@ def test(name: str, version: int):
             else:
                 action = np.random.randint(n_actions)
             observation, reward, terminated, truncated, info = env.step(action)
-            frames_observation.append(observation)
+            frames_observation.append(downscale(observation, 2))
 
 
 if __name__ == "__main__":
-    
-    #test("breakout", 2)
-    train("breakout")
+    test("space_invaders", 2)
+    #train("space_invaders", 2)
+    #plot_stats("space_invaders", 2)
